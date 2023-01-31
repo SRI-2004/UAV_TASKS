d91868d6a57a79395ab3580b7fe9b750027118d0
SRI-2004 <116736492+SRI-2004@users.noreply.github.com>
2023-01-31 17:51:28 +0530

    final

--- UAV_TASK3/UAV_TASK_CLIENT.py
+++ UAV_TASK3/UAV_TASK_CLIENT.py
@@ -1,21 +1,14 @@
 import socket
-
-# Create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Get local machine name
 host = socket.gethostname()
 
-# Reserve a port for your service
 port = 12345
 
-# Connect the socket to the server
 s.connect((host, port))
 
-# Receive the message from the server
 msg = s.recv(1024)
 
 print("Received message:", msg.decode("utf-8"))
 
-# Close the connection
 s.close()
